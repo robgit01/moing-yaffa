abcf830bf3f7da1983b0a631825031037f6c4110
adi <adi@adi-ThinkCentre-M92P>
2016-11-02 11:58:39 +0200

    Adding hen is the queen !!!

--- hello world/src/hello world.cpp
+++ hello world/src/hello world.cpp
@@ -12,5 +12,6 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "!!!Dani Shovevani!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hen Is The Queen!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
